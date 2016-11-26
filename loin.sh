436095d3f8b066b7105788fe9110a54ee0499894
ulmeyda <ulmeyda@creatale.de>
2016-11-26 18:04:33 +0100

    Fixed width columns

--- loin.sh
+++ loin.sh
@@ -8,8 +8,23 @@ function joinJsonContent {
 	jq '.[] | .title + "|" + .tags + "|" + .uri'
 }
 
+function limitColumnWidth {
+	awk 'BEGIN {FS = "|"}
+	{
+		print substr($1, 2, 30), "|", substr($2, 1, 30), "|", substr($3, 1, 30)
+	}'
+}
+
+function alignColumns {
+	column -t -s "|"
+}
+
 function searchBookmarks {
-	bookmarksAsJson | joinJsonContent | peco
+	bookmarksAsJson |
+	joinJsonContent |
+	limitColumnWidth |
+	alignColumns |
+	peco
 }
 
 function extractUrl {
