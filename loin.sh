928211c665c89e0a38a1970ffe9a3623fd60a121
ulmeyda <ulmeyda@creatale.de>
2016-11-26 18:04:55 +0100

    Add a few comments

--- loin.sh
+++ loin.sh
@@ -4,10 +4,13 @@ function bookmarksAsJson {
 	buku -p -j
 }
 
+# join all bookmark properties into one line
 function joinJsonContent {
 	jq '.[] | .title + "|" + .tags + "|" + .uri'
 }
 
+# cut off each column at a fixed width (awk is probably a bad choice,
+# use simpler tools)
 function limitColumnWidth {
 	awk 'BEGIN {FS = "|"}
 	{
@@ -31,8 +34,10 @@ function extractUrl {
 	egrep -o 'https?://[^ ]+'
 }
 
+# run everything and store peco output
 selectedUrl=$(searchBookmarks | extractUrl)
- 
+
+# open in browser if an url was found
 if [ -n "$selectedUrl" ];then
 	xdg-open $selectedUrl
 fi
